1acbbb98a03c42fb16ce147dc6bac9c308cb395b
Mauro Cubilla <mauroscubilla@gmail.com>
2023-09-09 20:15:56 -0300

    segunda practica

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export const config = {
+    server:{
+        port:8080,
+        secretSession:"secretCoderSessions"
+    },
+    mongo:{
+        url:"mongodb+srv://mauroscubilla:martiniracing2000@cluster0.sxupgx7.mongodb.net/segundaPractica?retryWrites=true&w=majority"
+    }
+}
